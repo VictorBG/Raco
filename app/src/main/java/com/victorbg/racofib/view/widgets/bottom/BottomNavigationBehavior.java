c0bef3ef6a53922fd7c3c94355214316ae01d01a
Victor <victorblancogarcia3@gmail.com>
2019-02-16 23:58:55 +0100

    Fixed a weird behavior when the FloatingActionBar of the main activity was hidden and the Snackbar was anchored to it.

--- app/src/main/java/com/victorbg/racofib/view/widgets/bottom/BottomNavigationBehavior.java
+++ app/src/main/java/com/victorbg/racofib/view/widgets/bottom/BottomNavigationBehavior.java
@@ -5,13 +5,20 @@ import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.victorbg.racofib.R;
 
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
 import androidx.core.view.ViewCompat;
-import timber.log.Timber;
 
+/**
+ * This behavior is applied to both items {@link com.google.android.material.bottomnavigation.BottomNavigationView}
+ * and {@link FloatingActionButton} in order to set the anchor dynamically, that involves
+ * a different placement of the snackbar when the {@link FloatingActionButton} is hidden or not.
+ *
+ * @param <V>
+ */
 public class BottomNavigationBehavior<V extends View> extends CoordinatorLayout.Behavior<V> {
 
     private float bottomMargin = 25;
@@ -35,7 +42,17 @@ public class BottomNavigationBehavior<V extends View> extends CoordinatorLayout.
     @Override
     public boolean layoutDependsOn(CoordinatorLayout parent, V child, View dependency) {
         if (dependency instanceof Snackbar.SnackbarLayout) {
-            updateSnackbar(child, (Snackbar.SnackbarLayout) dependency);
+            View v = parent.findViewById(R.id.fab);
+//            if (v != null && v.getVisibility() == View.VISIBLE) {
+//                updateSnackbar(v, (Snackbar.SnackbarLayout) dependency);
+//            } else {
+
+            if (child instanceof FloatingActionButton && child.getVisibility() == View.VISIBLE) {
+                updateSnackbar(child, (Snackbar.SnackbarLayout) dependency);
+            } else if (v.getVisibility() != View.VISIBLE) {
+                updateSnackbar(child, (Snackbar.SnackbarLayout) dependency);
+            }
+//            }
         }
 
         return super.layoutDependsOn(parent, child, dependency);
@@ -44,18 +61,19 @@ public class BottomNavigationBehavior<V extends View> extends CoordinatorLayout.
 
     private void updateSnackbar(View child, Snackbar.SnackbarLayout snackbarLayout) {
         if (snackbarLayout.getLayoutParams() instanceof CoordinatorLayout.LayoutParams) {
+
             CoordinatorLayout.LayoutParams params = (CoordinatorLayout.LayoutParams) snackbarLayout.getLayoutParams();
 
             params.setAnchorId(child.getId());
             params.anchorGravity = Gravity.TOP;
             params.gravity = Gravity.TOP;
-//            params.bottomMargin = (int) bottomMargin + child.getHeight();
-            params.bottomMargin = (int) bottomMargin;
+            params.bottomMargin = (int) bottomMargin + child.getHeight();
+//            params.bottomMargin = (int) bottomMargin;
             snackbarLayout.setLayoutParams(params);
-
-
         }
+
     }
-
-
 }
+
+
+
